0d7cc86f5aca2c67b3268cad7e14e4553bb2b3be
Marc Zalik <marc.zalik@gmail.com>
2022-04-12 08:16:49 -0500

    Initial Commit
    URL for Drawio diagram: https://viewer.diagrams.net/?tags=%7B%7D&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Untitled%20Diagram.drawio#R7VpZb9s4EP41etxAh%2BXj0UePxaZFtka36VPBSLTELSUKFB07%2FfU7lEhdVGJHazspWiBIxOEh8fu%2BoWZGsbxlsn%2FHURZ%2FYCGmlmuHe8tbWa7rOLYLf6TlobT49rQ0RJyEalBtWJMfWBltZd2SEOetgYIxKkjWNgYsTXEgWjbEOdu1h20Ybd81QxE2DOsAUdP6hYQiLq1T367t7zGJYlFtWPUkSA9WhjxGIds1TN4by1tyxkR5leyXmErwNC7lvLeP9FYPxnEqjpkwQdHn27%2FXX77t0n%2FE%2FV%2FfxpuPn%2F9Qq9wjulUbttwxhfUWIbmHy0hekjTE%2B6tYJFR3wl0a%2FWp%2F4kGDBjcGfqCx2MVE4HWGAtmzA4mArVjIWznVzOYe9ANhLvC%2BYVJ7eodZggV%2FgCGq1xsrfJXAnJkS2K6my9EcxA2qpsqGlEKiaukaRLhQOD4DU%2FcYTFdIoDsEGB2H6AEcUZ6V4t%2BQPQ5PBOy0DexoYhvATi%2BJ68jAdYkEjhgncEAsXWtu9%2FzuApnvSEJRKrW5YalYqx4b2kFMaHiNHthW7iQXKPiuW4sY7vIDxiONOHRzoY4rz26NWMuZak2Ocxhzo3lwOqYPaN8aeI1yoZ%2BGUYqynNwVzycnJohHJF0wIViiBp2A5urA0v4z6fGfUQ%2FPlVOdnGjfIPoTCD%2BXt0pD%2BL3e3vHS0CEXtiwKbjj7jpeMMg72lJVsE0o7JkRJlEKT4o2cJjEjcPLPlTkhYShXXuTgdySNrothq1Ft%2BaTQkCYG0ze0OOBjmIhhhUXGSCoKdPwF%2FABeS%2FvKt3x41iW0nboNP3I4F0uWwuMjUvCJQQ47LCWx4EzIE6MSq8n8kz5zWA6Kfs908l72vXORPza9nKI8b9MfaNNvAZxaAOOXFsDEEMD8jlAi5DOuA8Z%2F034O2mcvTfusJ2rqvrtjlMnLDcX7uYztAQ%2BchupyVRwKJNAvZ9PcCJg420JQGypQ8Z6IW3kNpJStr42e1b7ZeDj06s3Zlgf4cHwIzxjhpzh0FYk4bKUoJoVNzuy%2BN7U2ckyRIPft1KaPSXWPG6njRkQ4aYcK3qgjhnLralYzFeksNHI7C806C5XYGAsVwqo2PlxrOrl8OkRHWXYV5PmREXqtKOciec%2B4E7e59pF5z%2FhcHuz0JZNDXfhxX03hWW9VV9H4WnuubNbuWrS0v1Y%2B3vTwyt9P7eNaYQed3Hm%2Bj%2Fs9pPqn8fBRV1T%2BdKCH%2B4%2Bo81Ie7l1Wi1e2PWnq0TmkRtm6wZzARjF%2FEYnq18tBiU5etUQ9Z%2BhLqCvR7sF4boma9QxDoqeJbrRK7avZzGufmrbnneHc7FX3%2FzhMjxTq%2BFUL9ec9S816TF29hJQsxgkyD9e6znaCYMft1CL9qZmu9BYjvfNFO2aeWlapvDlYrQls2%2F6IEixTPteer%2F8sL95iJLZFDitb1yiNtigqihvWZGWg%2BOww8mkCj8e7U%2FrtQbsvOTwb2K4ZsOuqUC%2Fc74n0n5syDdcEqKsbzjYkIDgNiKahJuU1s1BF8y9HgxnhrzOY9wgLBUVK6Pi%2B%2BDD4iuF1%2B86Uy8I7NeGta52%2FjtBd%2B8WZOCJ9GJgqNDLVoxKFi5amNHWHa1PWaw61xt0q5dBQy%2B9%2BzDxzqOWaOYGsQv1rlrx18gosI0oxZRFHiawlN6LuVl8jHD9UnSo%2BIqtvm2f6Su%2BNPMPFL1utcs0PTY%2BCfPqC88DTYLhnH53tDyhIXbboPBnq252F3O5Cg30bmvW%2F8ZTD63%2BG8t78Bw%3D%3D

---
+++ database.js
@@ -0,0 +1,229 @@
+Races = [
+    {
+        name: "Aarakocra",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Dragonborn",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Hill Dwarf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Mountain Dwarf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Eladrin Elf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "High Elf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Wood Elf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Air Genasi",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Earth Genasi",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Fire Genasi",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Water Genasi",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Deep Gnome",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Rok Gnome",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Goliath",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Half-Elf",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Half-Orc",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Lightfoot Halfling",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Stout Halfling",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Human",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Variant Human",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Tiefling",
+        asis: ,
+        features: [],
+        languages: []
+    },
+    {
+        name: "Aasimar",
+        asis: ,
+        features: [],
+        languages: []
+    },
+]
+
+Classes = [
+    {
+        name: "Barbarian",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Bard",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Cleric",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Druid",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Fighter",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Monk",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Paladin",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Ranger",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Rogue",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Sorcerer",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Warlock",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    },
+    {
+        name: "Wizard",
+        hp: ,
+        asis: ,
+        proficiencies: [],
+        features: []
+    }
+]
+
+Subclasses = [
+
+]
+
+Spells = [
+
+]
